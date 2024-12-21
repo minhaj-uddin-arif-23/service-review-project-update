ac20b6a16f3e0735134322fe6d84bdbceefbe70e
Arif_1 <uarif1933@gmail.com>
2024-12-21 21:18:34 +0600

    Add service in database

--- src/pages/AddService.jsx
+++ src/pages/AddService.jsx
@@ -1,9 +1,109 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+export default function AddService() {
+const [startDate, setStartDate] = useState(new Date());
+const navigate = useNavigate()
+const handleService =async (e) => {
+e.preventDefault();
+const image = e.target.image.value;
+const title = e.target.title.value;
+const name = e.target.name.value;
+const website = e.target.website.value;
+const desc = e.target.desc.value;
+const category = e.target.category.value;
+const price = e.target.price.value;
+const email = e.target.email.value;
+const service = { image, title, name, website, desc, category,startDate, price, email };
+console.log(service);
 
-function AddService() {
-  return (
-    <div>AddService</div>
-  )
+
+  try{
+    await axios.post(`${import.meta.env.VITE_API_URL}/add-services`,service)
+    toast.success("Service added successfully")
+    navigate('/')
+  }catch(err){
+    toast.error(err,"Data not added")
+  }
+
+
+
+};
+
+return (
+<div>
+  <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+    <form onSubmit={handleService} className="card-body">
+      <div className="form-control">
+        <label className="label">
+          <span className="label-text">Service Image</span>
+        </label>
+        <input name="image" type="url" placeholder="Upload your image"
+          className="input input-bordered input-success w-full max-w-xs" required />
+      </div>
+      <div className="form-control">
+        <label className="label">
+          <span className="label-text">Service Title</span>
+        </label>
+        <input name="title" type="text" placeholder="title"
+          className="input input-bordered input-success w-full max-w-xs" required />
+      </div>
+      <div className="form-control">
+        <label className="label">
+          <span className="label-text">Company Name</span>
+        </label>
+        <input name="name" type="text" placeholder="name" className="input input-bordered input-success w-full max-w-xs"
+          required />
+      </div>
+      <div className="form-control">
+        <label className="label">
+          <span className="label-text">Website</span>
+        </label>
+        <input name="website" type="url" placeholder="website link"
+          className="input input-bordered input-success w-full max-w-xs" required />
+      </div>
+      <div className="form-control">
+        <label className="label">
+          <span className="label-text">Desciption</span>
+        </label>
+        <textarea name="desc" className="textarea textarea-accent" placeholder="Bio"></textarea>
+      </div>
+      <div>
+        <select name="category" className="select select-success w-full max-w-xs">
+          <option disabled selected>
+            Category
+          </option>
+          <option>One Piece</option>
+          <option>Naruto</option>
+          <option>Death Note</option>
+        </select>
+      </div>
+      <div className="form-control">
+        <label className="label">
+          <span className="label-text">Price</span>
+        </label>
+        <input name="price" type="number" placeholder="Price"
+          className="input input-bordered input-success w-full max-w-xs" required />
+      </div>
+      {/* date */}
+      <DatePicker selected={startDate} className="input input-bordered input-success w-full max-w-xs" onChange={(date)=>
+        setStartDate(date)} //only when value has changed
+        />
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input name="email" type="email" placeholder="email" // defaultValue={}
+            className="input input-bordered input-success w-full max-w-xs" required />
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Login</button>
+        </div>
+    </form>
+  </div>
+</div>
+);
 }
-
-export default AddService
